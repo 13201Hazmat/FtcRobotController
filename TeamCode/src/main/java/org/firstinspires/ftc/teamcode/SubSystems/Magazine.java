dd7e864c68cff3c9782f3e5f66971949d59b7514
amjadj <amjad.jabs@gmail.com>
2021-10-25 19:57:37 -0500

    S

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Magazine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Magazine.java
@@ -22,21 +22,20 @@ import com.qualcomm.robotcore.hardware.Servo;
  */
 public class Magazine {
 
-    //TODO: Update code as needed for Subsystem1
+    //TODO: Update code as needed for Magazine
 
-    public DcMotor subsystem1Motor = null;
     public Servo subsystem1Servo = null;
 
     public static final double SUBSYSTEM1_SERVO_LEVEL1 = 0.6;
     public static final double SUBSYSTEM1_SERVO_LEVEL2 = 0.35;
 
-    public enum SUBSYSTEM1_MOTOR_STATE {
+    public enum SUBSYSTEM1_SERVO_STATE {
         STATE1,
         STATE2,
         STATE3
     }
 
-    public SUBSYSTEM1_MOTOR_STATE subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE1;
+    public SUBSYSTEM1_SERVO_STATE subsystem1ServoState = SUBSYSTEM1_SERVO_STATE.STATE1;
 
     public double subsystem1MotorPower1 = 0.95;//0.9;
     public double subsystem1MotorPower2 = 0.8;
@@ -48,69 +47,31 @@ public class Magazine {
     public SUBSYSTEM1_BUTTON_STATE subsystem1ButtonState;
 
     public Magazine(HardwareMap hardwareMap) {
-        subsystem1Motor = hardwareMap.dcMotor.get("frmotor");
-        //subsystem1Servo = hardwareMap.servo.get("servotest");
+        subsystem1Servo = hardwareMap.servo.get("servotest");
     }
 
-    public void initSubsystem1(){
+    public void initMagazine(){
 
-    }
-
-    /**
-     * runIntakeMotor checks if the intake is not running and runs the intake
-     */
-    public void startForwardSubsystem1Motor() {
-        if(subsystem1MotorState != SUBSYSTEM1_MOTOR_STATE.STATE1) {
-            runSubsystem1Motor(DcMotor.Direction.REVERSE, subsystem1MotorPower1);
-            subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE1;
-        }
-    }
-
-    /**
-     * stopIntakeMotor checks if the intake has stopped and if its not, it sets the intake power to 0
-     * and sets intakeMotorState to INTAKE_MOTOR_STATE.STOPPED
-     */
-    public void stopSubsystem1Motor() {
-        if(subsystem1MotorState != SUBSYSTEM1_MOTOR_STATE.STATE2) {
-            runSubsystem1Motor(DcMotor.Direction.FORWARD, 0.0);
-            subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE2;
-       }
-    }
-
-    /**
-     * reverseIntakeMotor checks if the intake is not reversing, and sets the intake motor to FORWARD, then also
-     * ets intake motor state to REVERSING
-     */
-    public void startReverseSubsystem1Motor() {
-        if(subsystem1MotorState != SUBSYSTEM1_MOTOR_STATE.STATE3) {
-            runSubsystem1Motor(DcMotor.Direction.FORWARD, subsystem1MotorPower2);
-            subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE3;
-        }
-    }
-
-    private void runSubsystem1Motor(DcMotor.Direction direction, double power){
-        subsystem1Motor.setDirection(direction);
-        subsystem1Motor.setPower(power);
     }
 
     /**
      * set Intake gripper position to hold.. to ensure intake is within robot dimensions at start
      */
-    public void setIntakeReleaseHold(){
+    public void setSubsystem1ServoLevel1(){
         subsystem1Servo.setPosition(SUBSYSTEM1_SERVO_LEVEL1);
     }
 
     /**
      * set Intake gripper position to release
      */
-    public void setIntakeReleaseOpen(){
+    public void setSubsystem1ServoLevel2(){
         subsystem1Servo.setPosition(SUBSYSTEM1_SERVO_LEVEL2);
     }
 
     /**
      * Returns Intake motor state
      */
-    public SUBSYSTEM1_MOTOR_STATE getSubsystemMotorState() {
-        return subsystem1MotorState;
+    public SUBSYSTEM1_SERVO_STATE getSubsystemMotorState() {
+        return subsystem1ServoState;
     }
 }
